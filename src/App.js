de15dfba046f82d92b86c3b7afe4c3f455839f88
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-12 11:39:55 -0300

    search animation div colors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
 
   const [city, setCity] = useState('');
   const [weatherForecast, setWeatherForeCast] = useState('');
-  const [background, setBackground] = useState('mt-5 main-container')
+  const [background, setBackground] = useState('mt-5 main-container');
+  const [initialSpace, setInitialSpace] = useState('initial-space')
 
   const handleChange = (e) =>{
     setCity(e.target.value);
@@ -20,6 +21,7 @@ function App() {
     })
     .then((data) => {
       setWeatherForeCast(data);
+      setInitialSpace('initial-space active-out');
       console.log('data ===>', data);
       if(data.current.temp_c > 20){
       setBackground('mt-5 main-container row yellow');
@@ -28,56 +30,49 @@ function App() {
       } 
     });
   };
-
+  
   return (
-    <div className={background}>
+    <div className={background}>        
+    <div className={initialSpace}></div>
       {weatherForecast ? (
           <div className='main-container-active'>
 
             <div className='temperature-box mt-3'>
-    
-            <p>{weatherForecast.location.region}</p>
-              <small className='temp-text '>
-                {weatherForecast.current.temp_c + "°c"}
-              </small>
-              <img src={weatherForecast.current.condition.icon} className='weather-condition mb-4' alt='ícone que representa o clima atual da aplicação'/>
-            <p>{weatherForecast.current.condition.text}</p>        
-
+              <p>{weatherForecast.location.region}</p>
+                <small className='temp-text '>
+                  {weatherForecast.current.temp_c + "°c"}
+                </small>
+                <img src={weatherForecast.current.condition.icon} className='weather-condition mb-4' alt='ícone que representa o clima atual da aplicação'/>
+              <p>{weatherForecast.current.condition.text}</p>        
             </div>
           
             <div className='icons-container mt-3'>
 
               <div className='current-icons'>
                 <img src={require('./assets/images/temperaturec.png')} className='weather-icons' alt='Medidor de temperatura em graus mais conhecido como Termômetro'/>
-               
-                  {weatherForecast.current.temp_c + "°c"}
-               
+                {weatherForecast.current.temp_c + "°c"}
               </div>
               
               <div className='current-icons'>
                 <img src={require('./assets/images/wind.png')} className='weather-icons' alt='imagem que representa o vento se movimentando'/>
-                
-                    {weatherForecast.current.wind_kph + "km/h"}
-                 
+                {weatherForecast.current.wind_kph + "km/h"}
               </div>
 
               <div className='current-icons'>
               <img src={require('./assets/images/water.png')} className='weather-icons' alt='imagem que representa o vento se movimentando'/>
-              
-                  {weatherForecast.current.humidity + '%'}
-               
+                {weatherForecast.current.humidity + '%'}
               </div>
 
             </div>
 
           </div>
-            ) : <div className='empty-space'></div>}
-
-
+            ) :  null}
+            
       <div className='search-container'>
               <input type="text" className="weather-input d-block mt-4" onChange={handleChange} placeholder="Insira sua cidade aqui"/>
-              <button onClick={handleSearch} className="bg-success text-white search-button d-block mt-3" type="button">Buscar</button>
+              <button onClick={handleSearch} className="text-white search-button d-block mt-3" type="button">Buscar</button>
       </div>
+    
     </div>
   );
 }
