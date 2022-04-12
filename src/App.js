28c2f881cc9503331762f0c27448b006cfeae838
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-12 13:01:04 -0300

    icon container temperature box animation onclick

--- src/App.js
+++ src/App.js
@@ -6,38 +6,53 @@ function App() {
   const [city, setCity] = useState('');
   const [weatherForecast, setWeatherForeCast] = useState('');
   const [background, setBackground] = useState('mt-5 main-container');
-  const [initialSpace, setInitialSpace] = useState('initial-space')
+  const [initialSpace, setInitialSpace] = useState('initial-space');
+  const [temperatureBox, setTemperature] = useState('temperature-box mt-3');
+  const [iconContainer, setIconContainer] = useState('icons-container mt-3')
+
 
   const handleChange = (e) =>{
     setCity(e.target.value);
   }
   const handleSearch = () =>{
+    setInitialSpace('initial-space active-out');
+    setIconContainer('icons-container mt-3 boxOut');
+    setTemperature('temperature-box mt-3 boxOut');
+    setTimeout(() => {
+      setTemperature('temperature-box mt-3');
+      setIconContainer('icons-container mt-3')
+    }, 1000);
+
+    setIconContainer('icons-container mt-3 boxOut');
     fetch(
       `http://api.weatherapi.com/v1/current.json?key=4e77fbf9672f4409ba6184750220704&q=${city}&lang=pt`
     ).then((response) => {
       if(response.status === 200){
         return response.json()
       }
+      if(response.status === 400){
+        return console.log('esse valor não foi encontrado');
+      }
     })
     .then((data) => {
       setWeatherForeCast(data);
-      setInitialSpace('initial-space active-out');
+     
       console.log('data ===>', data);
       if(data.current.temp_c > 20){
       setBackground('mt-5 main-container row yellow');
-      } else{
+      } else {
         setBackground('mt-5 main-container row blue');
-      } 
+      }
     });
   };
-  
+
   return (
     <div className={background}>        
     <div className={initialSpace}></div>
       {weatherForecast ? (
           <div className='main-container-active'>
 
-            <div className='temperature-box mt-3'>
+            <div className={temperatureBox}>
               <p>{weatherForecast.location.region}</p>
                 <small className='temp-text '>
                   {weatherForecast.current.temp_c + "°c"}
@@ -46,7 +61,7 @@ function App() {
               <p>{weatherForecast.current.condition.text}</p>        
             </div>
           
-            <div className='icons-container mt-3'>
+            <div className={iconContainer}>
 
               <div className='current-icons'>
                 <img src={require('./assets/images/temperaturec.png')} className='weather-icons' alt='Medidor de temperatura em graus mais conhecido como Termômetro'/>
